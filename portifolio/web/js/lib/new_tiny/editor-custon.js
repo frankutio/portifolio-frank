beeb6fc8c571a01a06c41442faf5576b255f057a
Frank Bezerra Simiao <frankutio@gmail.com>
2018-09-21 10:12:02 -0300

    Add plugin do editor moderno

---
+++ portifolio/web/js/lib/new_tiny/editor-custon.js
@@ -0,0 +1,13 @@
+tinyMCE.init({
+    
+    selector: '.rich-text',
+    language: 'pt_BR',
+    theme: 'modern',
+    plugins: [
+      'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+      'save table contextmenu directionality emoticons template paste textcolor'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+  
+});
