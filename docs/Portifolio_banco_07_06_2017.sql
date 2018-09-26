c263481ce307589aa943b1983b4a1731e57b3741
Frank Bezerra <frankutio@gmail.com>
2018-09-26 15:21:21 -0300

    Ajuste no script de restauração do banco de dados

--- docs/Portifolio_banco_07_06_2017.sql
+++ docs/Portifolio_banco_07_06_2017.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE `portifolio` CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `portifolio`;
+
 --
 -- Table structure for table `image`
 --
